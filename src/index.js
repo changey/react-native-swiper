62513cb77b7fee74dec8ddf195801281641a63a4
Eric Chang <eric@goforward.com>
2016-11-15 01:56:26 -0800

    increase margin

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ import {
 const { width, height } = Dimensions.get('window')
 
 const CARD_PREVIEW_WIDTH = 20;
-const CARD_MARGIN = 5;
-const CARD_WIDTH = Dimensions.get('window').width * 0.95 - (CARD_MARGIN + CARD_PREVIEW_WIDTH) * 2;
+const CARD_MARGIN = 10;
+const CARD_WIDTH = Dimensions.get('window').width * 0.9 - (CARD_MARGIN + CARD_PREVIEW_WIDTH) * 2;
 
 /**
  * Default styles
